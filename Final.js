90e66587f8baae6eeaefbf1c68cf4cc1774f86d7
Prayrana Khadye <103586617+pksuperduper@users.noreply.github.com>
2024-02-07 18:26:58 -0500

    Update Final.js

--- Final.js
+++ Final.js
@@ -4,20 +4,12 @@
 
 function goMadLib(formData) { 
 
-    let noun = formData.elements.namedItem("noun").value;
-    let activity = formData.elements.namedItem("activity").value;
-    let different = formData.elements.namedItem("different").value;
-    let final = formData.elements.namedItem("final").value;
-    
-    let newContent = "I found myself in a " + noun + ".";
+const football = ["49s", "Chiefs"];
 
-    newContent += "I wanted to know what I should do and I did this -- " + activity + ".";
-
-    newContent += "Then, I decided to do something different and " + different + ".";
-    
-    newContent += "Finally, I did this --" + final + ".";
-    
-    document.getElementById("content").innerHTML = newContent;
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += football[i] + "<br>";
+}
     
 }
 	
